4b91b4742be49e843dd5d3b548f0a623b99290c8
Jadoson <ghost1342@gmail.com>
2024-09-09 17:01:42 +0300

    Prepare for GitHub Pages deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  target: 'static',
+  router: {
+    base: '/servermanager/',
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
 })
